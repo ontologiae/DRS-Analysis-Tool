245e9c5cebb274ac1a97b97bc1dae9282391c1b8
Pierre-Alex <ontologiae@gmail.com>
2012-10-27 15:46:15 +0200

    Improving match case in objet_en_tant_que_sujet

--- paraphraser.ml
+++ paraphraser.ml
@@ -130,28 +130,35 @@ paraphraser_atom l  false (PredicateDiTransitive (Var "D", Verbe "give", Var "A"
 
 *)
 
-let objet_en_tant_que_sujet (Object(  ref, Nom name,  countable,  unittype,  op, Number count,x,y) ) =
-        match countable, unittype, op with
-        | Countable, Na        , o    ->  ( match o with 
-                                             | Eq  -> let txt = (string_of_int count)^" "^name in if count < 2 then txt^" " else txt^"s "
-                                             | Geq -> "At least "^(string_of_int count)^" "^name^" "
-                                             | Greater -> "More than "^(string_of_int count)^" "^name^" "
-                                             | Leq     -> "Less or equal than "^(string_of_int count)^" "^name^" "
-                                             | Less    -> "Less than "^(string_of_int count)^" "^name^" "
-                                             | Exactly -> "Exactly "^(string_of_int count)^" "^name^" "
-                                             | NaOp    -> " "^name^" "
-                                        )
-        | Dom      , Na       , NaOp  -> name^" "
-        | Mass     , Kg       , o     -> ( match o with 
-                                             | Eq  -> (string_of_int count)^" kg "
-                                             | Geq -> "At least "^(string_of_int count)^" kg "
-                                             | Greater -> "More than "^(string_of_int count)^" kg "
-                                             | Leq     -> "Less or equal than "^(string_of_int count)^" kg "
-                                             | Less    -> "Less than "^(string_of_int count)^" kg "
-                                             | Exactly -> "Exactly "^(string_of_int count)^" kg "
-                                             | NaOp    -> failwith "illogique Na avec une masse"
-                                        )
-        | _ , _ , _ -> failwith "Cas objet non traité";;
+let objet_en_tant_que_sujet obj =
+        match obj with
+        | Object(  ref, Nom name,  countable,  unittype,  op, Number count,x,y)  ->
+                        (match countable, unittype, op with
+                        | Countable, Na        , o    ->  ( match o with 
+                        | Eq  -> let txt = (string_of_int count)^" "^name in if count < 2 then txt^" " else txt^"s "
+                        | Geq -> "At least "^(string_of_int count)^" "^name^" "
+                        | Greater -> "More than "^(string_of_int count)^" "^name^" "
+                        | Leq     -> "Less or equal than "^(string_of_int count)^" "^name^" "
+                        | Less    -> "Less than "^(string_of_int count)^" "^name^" "
+                        | Exactly -> "Exactly "^(string_of_int count)^" "^name^" "
+                        | NaOp    -> " "^name^" "
+                        )
+                        | Dom      , Na       , NaOp  -> name^" "
+                        | Mass     , Kg       , o     -> ( match o with 
+                        | Eq  -> (string_of_int count)^" kg "
+                        | Geq -> "At least "^(string_of_int count)^" kg "
+                        | Greater -> "More than "^(string_of_int count)^" kg "
+                        | Leq     -> "Less or equal than "^(string_of_int count)^" kg "
+                        | Less    -> "Less than "^(string_of_int count)^" kg "
+                        | Exactly -> "Exactly "^(string_of_int count)^" kg "
+                        | NaOp    -> failwith "illogique Na avec une masse"
+                        )
+                        | _ , _ , _ -> failwith "Cas objet non traité"
+                        )
+         | Named nom  -> nom^" "
+         | String nom -> nom^" "
+
+;;
 
 
 
@@ -160,28 +167,28 @@ let paraphrase_phrase_simple lst =
         let verbe = trouve_verbe lst in
         match verbe with
         | PredicateIntransitive ( ref , Verbe verb, SubAtom subject, gramnbr    )               ->  (objet_en_tant_que_sujet subject)^(verb2str verb gramnbr)
-                                                                                               
+
         | PredicateTransitive   ( ref , Verbe verb, SubAtom subject, SubAtom cod, gramnbr )      ->  (objet_en_tant_que_sujet subject)^(verb2str verb gramnbr)^(objet_en_tant_que_sujet cod)
 
-                                                                                               
+
         | PredicateDiTransitive ( ref , Verbe verb, SubAtom subject, SubAtom cod, SubAtom coi, gramnbr )  ->  
                         (objet_en_tant_que_sujet subject)^(verb2str verb gramnbr)^(objet_en_tant_que_sujet cod)^"to "^(objet_en_tant_que_sujet coi)
         | _ -> failwith "cas paraphrase non traité";;
-                                                                                               
+
 let aiguillage_phrase l = match  exists_Predicate l, exists_Modifier_pp l, exists_Relation l with
-                                |  true, false,false   -> "Phrase simple"
-                                |  true, true,false    -> "Phrase avec modifiers_pp"
-                                |  false, false,false  -> "Phrase nominale"
-                                |  true, false,false   -> "Phrase intransitive (genre 'Il pleut')"
-                                |  true, true, true    -> "Phrase simple avec modifiers_pp et relation (of)"
-                                |  false, _ ,_         -> "Cas non géré pour le moment, ou illogique";;
-                
+|  true, false,false   -> "Phrase simple"
+|  true, true,false    -> "Phrase avec modifiers_pp"
+|  false, false,false  -> "Phrase nominale"
+|  true, false,false   -> "Phrase intransitive (genre 'Il pleut')"
+|  true, true, true    -> "Phrase simple avec modifiers_pp et relation (of)"
+|  false, _ ,_         -> "Cas non géré pour le moment, ou illogique";;
+
 
 
 let g = FullDRS ([Var "J1"; Var "K1"; Var "L1"],                                                                                              
-   [Object (Var "J1", Nom "time", Countable, Na, Greater, Number 2, 13, 8);                                                             
-    Object (Var "K1", Nom "day", Countable, Na, Eq, Number 10, 13, 13);
-    PredicateTransitive (Var "L1", Verbe "vote", SubAtom (Named "User1"),
-     Var "J1", Singular);
-    Modifier_pp (Var "L1", Preposition "in", Var "K1");
-    Modifier_pp (Var "L1", Preposition "for", SubAtom (Named "User2"))]);;
+[Object (Var "J1", Nom "time", Countable, Na, Greater, Number 2, 13, 8);                                                             
+Object (Var "K1", Nom "day", Countable, Na, Eq, Number 10, 13, 13);
+PredicateTransitive (Var "L1", Verbe "vote", SubAtom (Named "User1"),
+Var "J1", Singular);
+Modifier_pp (Var "L1", Preposition "in", Var "K1");
+Modifier_pp (Var "L1", Preposition "for", SubAtom (Named "User2"))]);;
